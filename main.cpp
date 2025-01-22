13ad92ff7447a4059c0e2c64a68975c922aa21c9
sssh <shihar1994@gmail.com>
2025-01-22 10:14:49 +0530

    Initial Commit and Project Backbone creation

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <librdkafka/rdkafka.h>
+#include <librdkafka/rdkafkacpp.h>
+#include <string>
+
+#include <pthread/pthread.h>
+
+
+int main(int, char**){
+    pthread_t ptid; 
+    
+    std::cout << "Hello, from kafka_tool!\n";
+}
